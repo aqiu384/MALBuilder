f5f7fb6fbefbe66e8d55b74d2d4e4bbc8c80e81f
Andrew Nguyen <anguyn10@illinois.edu>
2015-02-19 06:22:07 -0600

    Basic search functionality painfully parsed together

--- API/aasession.py
+++ API/aasession.py
@@ -1,3 +1,5 @@
+import json
+import urllib.request as ureq
 class AaEntry():
 
     def __init__(self, malid, title):
@@ -14,6 +16,7 @@ class AaSession:
         'CHAPTERS': 'chapters',
         'EPISODES': 'episodes',
         'LENGTH': 'length',
+        'TITLE': 'title'
     }
 
     QUERY_OUTPUTS = {
@@ -44,6 +47,28 @@ class AaSession:
 
     def searchanime(self, sparams, rparams):
         results = []
-        results.append(AaEntry(1, 'Cowboy Bebop'))
-        results.append(AaEntry(2, 'Berserk'))
+        url = "http://test.animeadvice.me/api/v1/animelist/?filters={\"$and\":["
+        for word in sparams:
+            if word == "title":
+                url = url + "{\"title\":{\"$regex\":\"(?=.*" + sparams[word] + ")\",\"$options\":\"i\"},"
+            else:
+                url = url + "{\"" + word + "\":{\"$gte\":" + sparams[word] + ",\"$lte\":" + sparams[word] + "},"
+        url = url[:-1]
+        url = url + "}]}&fields=[\"title\"]" + "&limit=0&query_limit=50&group_limit=100&sort_type=max&format=json"
+        #print(url)
+        data = json.loads(ureq.urlopen(url).read().decode())
+        #print(data)
+        for entry in data['objects']:
+            results.append(AaEntry(entry['_id']['i'],entry['title']))
         return results
+
+    def convertToReadable(self, input):
+        input = input.replace("%7B", "{")
+        input = input.replace("%22", "\"")
+        input = input.replace("%24", "$")
+        input = input.replace("%3A", ":")
+        input = input.replace("%5B", "[")
+        input = input.replace("%7D", "}")
+        input = input.replace("%5D", "]")
+        input = input.replace("%2C", ",")
+        print(input)
