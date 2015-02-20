256e75fecb4cbad2389cff1d584c09c51bb1ecd0
Jaily <jlzeng2@illinois.edu>
2015-02-19 19:05:51 -0600

    mal api

--- API/malinterface.py
+++ API/malinterface.py
@@ -8,66 +8,76 @@ class MyAnimeListInterface(cmd.Cmd):
     prompt = "(MALI): "
 
     def __init__(self):
-        super(AnimeAdviceInterface, self).__init__()
+        super(MyAnimeListInterface, self).__init__()
         self.session = None
 
     def cmdloop(self):
         print('MAL credentials required for login.')
         username = input('Username: ')
-        password = getpass('Password: ')
+        password = getpass.getpass('Password: ')
         self.session = MalSession(username, password)
         return cmd.Cmd.cmdloop(self)
 
     def do_view(self, line):
-        '''View current MAL'''
+        """View current MAL"""
         print('Current MAL:')
 
-    def do_searchkey(self, line):
-        '''Search anime by keyword'''
-        if line == "":
-            print('Usage: searchkey [keyword]')
-            return
+    # def do_searchkey(self, line):
+    #     """Search anime by keyword"""
+    #     self.session.searchkey(line)
+
+    def do_searchtitle(self, line):
+        line = line.strip()
         print('Search for "{}" returned:'.format(line))
+        self.session.searchtitle(line)
 
     def do_searchid(self, line):
-        '''Search anime by ID'''
+        """Search anime by ID"""
         try:
             id = int(line)
+            self.session.searchid(id)
             print('Description for {}'.format(id))
         except ValueError:
-            print('Usage: searchkey [mal_id]')
+            print('Usage: searchid [mal_id]')
 
     def do_add(self, line):
-        '''Add a new anime given the ID'''
+        """Add a new anime given the ID"""
         try:
             args = line.split()
             id = int(args[0])
             status = args[1]
             print('Adding "{:d}" with watch status "{:s}" to MAL.'.format(id, status))
+            self.session.add(id, status)
         except (ValueError, IndexError):
             print('Usage: add [mal_id] [watch status]')
 
     def do_update(self, line):
-        '''Update an existing anime given the ID'''
+        """Update an existing anime given the ID"""
         try:
             args = line.split()
             id = int(args[0])
             metric = args[1]
             value = args[2]
             print('Updating "{:d}" with "{:s}: {:s}" in MAL.'.format(id, metric, value))
+            self.session.update(id, metric, value)
         except (ValueError, IndexError):
             print('Usage: update [mal_id] [metric] [value]')
 
     def do_delete(self, line):
-        '''Delete an exiting anime given the ID'''
+        """Delete an exiting anime given the ID"""
         try:
             id = int(line)
             print('Deleting {}'.format(id))
+            self.session.delete(id)
         except ValueError:
             print('Usage: delete [mal_id] from MAL.')
 
     def do_exit(self, line):
-        '''Exits AAI'''
+        """Exits AAI"""
+        return True
+
+    def do_EOF(self, line):
+        """Exits AAI"""
         return True
 
 if __name__ == '__main__':
--- API/malsession.py
+++ API/malsession.py
@@ -1,8 +1,83 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from xml.etree import ElementTree
+import urllib
+
+proxies = {
+    "http": "http://localhost:8000",
+    "https": "http://localhost:8000",
+}
+headers = {
+'User-Agent': 'api-taiga-32864c09ef538453b4d8110734ee355b',
+'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+data = 'data=%3Centry%3E%0A%09%3Cstatus%3E6%3C%2Fstatus%3E%0A%3C%2Fentry%3E%0A'
+testusername = 'Bill900'
+testpassword = 'testpw'
+xml = '<?xml version="1.0" encoding="UTF-8" ?><entry><status>2</status></entry>'
+auth = HTTPBasicAuth(testusername, testpassword)
+
+#not xml
+searchURL = 'http://myanimelist.net/api/anime/search.xml?q=' #[]
+addURL = 'http://myanimelist.net/api/animelist/add/' #[id].xml
+deleteURL = 'http://myanimelist.net/api/animelist/delete/' #[id].xml
+updateURL = 'http://myanimelist.net/api/animelist/update/' #[id].xml
+# no auth needed for ajax
+searchAjaxURL = 'http://myanimelist.net/includes/ajax.inc.php?id='
+searchAjaxMagicNumber = '&t=64'
+
+
 class MalSession:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
         # Authenticate username with password through MAL
-        if username != password:
-            raise Exception('Invalid MAL credentials')
+        # if username != password:
+        #     raise Exception('Invalid MAL credentials')
+
+    def searchtitle(self, title):
+        r = requests.post(searchURL + title, auth=auth, headers=headers, data=data)
+        if (r.text).find(title):
+            print("Success!")
+        else:
+            print("Fail")
+
+    # def searchkey(self, keywords):
+    #     try:
+    #         if keywords == "":
+    #             raise IndexError
+    #         keywords = keywords.split(',')
+    #         search = ""
+    #         for key in keywords:
+    #             key = key.strip()
+    #             search += key + ','
+    #         search = search[:-1]
+    #         print('Search for "{}" returned:'.format(search))
+        # do searching
+        # except(IndexError):
+        #     print('Usage: searchkey [keyword(s),]')
+        # return ""
+
+    def searchid(self, id):
+        r = requests.post(searchAjaxURL + str(id) + searchAjaxMagicNumber, headers=headers, data=data)
+
+    def add(self, id, status):
+        r = requests.post(addURL + str(id) + '.xml', auth=auth, headers=headers, data=data)
+        # e = ElementTree.XML(r.content)
+
+        if (r.content).find("This anime is already on your list.") != -1:
+            print("Failed: This anime is already on your list.")
+        elif r.status_code == '201':
+            print("Added")
+
+    def update(self, id, metric, status):
+        r = requests.post(updateURL + str(id) + '.xml', auth=auth, headers=headers, data=data)
+
+    def delete(self, id):
+        r = requests.post(deleteURL + str(id) + '.xml', auth=auth, headers=headers, data=data)
+        if (r.text).find("Deleted") or (r.status_code == '200'):
+            print("Deleted")
+        else:
+            print("Fail to delete")
