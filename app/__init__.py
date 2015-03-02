673740f0b80011e7bca5d76c5ce1e6351bf1fb87
Allen <aqiu2@illinois.edu>
2015-03-02 13:06:15 -0600

    Added login functionality in preparation for MAL session

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 # from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
 app.config.from_object('config')
 # #heroku = Heroku(app)
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
 from app import views, models
--- app/forms.py
+++ app/forms.py
@@ -1,34 +1,53 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, SelectMultipleField, IntegerField, DateField, DecimalField
+from wtforms import StringField, BooleanField, SelectMultipleField, IntegerField, DateField, DecimalField, PasswordField
 from wtforms.validators import DataRequired, Optional, NumberRange
-from api.aasession import AA_GENRE, AA_STATUS, AA_FIELD_TO_TYPE, AA_TYPE
+from .models import AA_TYPE, AA_STATUS, ANIME_ATTRS
 
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    rememberMe = BooleanField('Remember me', default=False)
 
 
-class AaSearchForm(Form):
-    idiStart = IntegerField('MAL ID start', validators=[Optional()])
-    idiEnd = IntegerField('MAL ID end', validators=[Optional()])
-    idt = SelectMultipleField('Status', choices=list(zip(AA_TYPE, AA_TYPE)), validators=[Optional()])
+# class AaSearchForm(Form):
+#     idiStart = IntegerField('MAL ID start', validators=[Optional()])
+#     idiEnd = IntegerField('MAL ID end', validators=[Optional()])
+#     idt = SelectMultipleField('Status', choices=list(zip(AA_TYPE, AA_TYPE)), validators=[Optional()])
+#     title = StringField('Title keyword', validators=[Optional()])
+#     airedFromStart = DateField('Aired from start', format='%m/%d/%Y', validators=[Optional()])
+#     airedFromEnd = DateField('Aired from end', format='%m/%d/%Y', validators=[Optional()])
+#     airedToStart = DateField('Aired to start', format='%m/%d/%Y', validators=[Optional()])
+#     airedToEnd = DateField('Aired to end', format='%m/%d/%Y', validators=[Optional()])
+#     episodesStart = IntegerField('Episodes start', validators=[Optional()])
+#     episodesEnd = IntegerField('Episodes end', validators=[Optional()])
+#     durationStart = IntegerField('Duration start', validators=[Optional()])
+#     durationEnd = IntegerField('Duration end', validators=[Optional()])
+#     genresInclude = SelectMultipleField('Gen include', choices=list(zip(AA_GENRE, AA_GENRE)), validators=[Optional()])
+#     genresExclude = SelectMultipleField('Gen exclude', choices=list(zip(AA_GENRE, AA_GENRE)), validators=[Optional()])
+#     malScoreStart = DecimalField('MAL score start', validators=[NumberRange(1, 10), Optional()])
+#     malScoreEnd = DecimalField('Mal score end', validators=[NumberRange(1, 10), Optional()])
+#     membersStart = IntegerField('Members ID start', validators=[Optional()])
+#     membersEnd = IntegerField('Members ID end', validators=[Optional()])
+#     statusInclude = SelectMultipleField('Status', choices=list(zip(AA_STATUS, AA_STATUS)), validators=[Optional()])
+#
+#     returnFields = SelectMultipleField('Return fields', choices=list(zip(AA_FIELD_TO_TYPE, AA_FIELD_TO_TYPE)),
+#                                        validators=[DataRequired()])
+
+
+class AnimeSearchForm(Form):
+    malIdStart = IntegerField('MAL ID start', validators=[Optional()])
+    malIdEnd = IntegerField('MAL ID end', validators=[Optional()])
+    type = SelectMultipleField('Type', choices=list(AA_TYPE.items()), coerce=int, validators=[Optional()])
+    status = SelectMultipleField('Status', choices=list(AA_STATUS.items()), coerce=int, validators=[Optional()])
     title = StringField('Title keyword', validators=[Optional()])
-    airedFromStart = DateField('Aired from start', format='%m/%d/%Y', validators=[Optional()])
-    airedFromEnd = DateField('Aired from end', format='%m/%d/%Y', validators=[Optional()])
-    airedToStart = DateField('Aired to start', format='%m/%d/%Y', validators=[Optional()])
-    airedToEnd = DateField('Aired to end', format='%m/%d/%Y', validators=[Optional()])
-    episodesStart = IntegerField('Episodes start', validators=[Optional()])
-    episodesEnd = IntegerField('Episodes end', validators=[Optional()])
-    durationStart = IntegerField('Duration start', validators=[Optional()])
-    durationEnd = IntegerField('Duration end', validators=[Optional()])
-    genresInclude = SelectMultipleField('Gen include', choices=list(zip(AA_GENRE, AA_GENRE)), validators=[Optional()])
-    genresExclude = SelectMultipleField('Gen exclude', choices=list(zip(AA_GENRE, AA_GENRE)), validators=[Optional()])
-    malScoreStart = DecimalField('MAL score start', validators=[NumberRange(1, 10), Optional()])
-    malScoreEnd = DecimalField('Mal score end', validators=[NumberRange(1, 10), Optional()])
-    membersStart = IntegerField('Members ID start', validators=[Optional()])
-    membersEnd = IntegerField('Members ID end', validators=[Optional()])
-    statusInclude = SelectMultipleField('Status', choices=list(zip(AA_STATUS, AA_STATUS)), validators=[Optional()])
+    startDateStart = DateField('Aired from start', format='%m/%d/%Y', validators=[Optional()])
+    startDateEnd = DateField('Aired from end', format='%m/%d/%Y', validators=[Optional()])
+    endDateStart = DateField('Aired to start', format='%m/%d/%Y', validators=[Optional()])
+    endDateEnd = DateField('Aired to end', format='%m/%d/%Y', validators=[Optional()])
+    scoreStart = DecimalField('MAL score start', validators=[NumberRange(1, 10), Optional()])
+    scoreEnd = DecimalField('Mal score end', validators=[NumberRange(1, 10), Optional()])
+    membersStart = IntegerField('Members count start', validators=[Optional()])
+    membersEnd = IntegerField('Members count end', validators=[Optional()])
 
-    returnFields = SelectMultipleField('Return fields', choices=list(zip(AA_FIELD_TO_TYPE, AA_FIELD_TO_TYPE)),
-                                       validators=[DataRequired()])
+    fields = SelectMultipleField('Return fields', choices=list(ANIME_ATTRS.items()), validators=[DataRequired()])
