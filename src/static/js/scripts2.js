caf5fd9b7853dfbd7acf9e799ac222885611c7ba
aqiu384 <aqiu384@gmail.com>
2015-03-17 00:38:07 -0500

    Enabled next search results loading on add anime

--- src/static/js/scripts2.js
+++ src/static/js/scripts2.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
-    $('#searchanimebutton').bind('click', function(event) {
-        event.preventDefault();
-        console.log("hi");
-        $.ajax({
-            type: "POST",
-            url: "addanime",
-            data: $('#animesearchform').serialize(),
-            success: function(returnData) {
-                $('#addanimeform').empty();
-                $('#searchformpage').replaceWith(returnData);
-                },
-            return: function(failData) {
-                console.log(failData);
-                }
-        });
-    });
-
-    $('#animesearchbutton').bind('click', function(event) {
+    $('#addanimebutton').bind('click', function(event) {
         event.preventDefault();
         $.ajax({
             type: "POST",
@@ -24,22 +7,8 @@ $(document).ready(function(){
             data: $('#addanimeform').serialize(),
             success: function(returnData) {
                 //$('#searchformpage').replaceWith(returnData);
+                $('addanimeform').replaceWith(returnData)
                 console.log("success");
-                },
-            return: function(failData) {
-                console.log(failData);
-                }
-        });
-        $.ajax({
-            type: "POST",
-            url: "addanime",
-            data: $('#animesearchform').serialize(),
-            success: function(returnData) {
-                console.log("second")
-                $('#addanimeform').empty();
-                $('#searchformpage').replaceWith(returnData);
-                var element = document.getElementById("addanimeform");
-                element.scrollIntoView({block: "start", behavior: "smooth"});
                 },
             return: function(failData) {
                 console.log(failData);
