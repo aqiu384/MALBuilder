42b8f802cbe97a64d63e06bbadb431ec69fc7321
Jaily <jlzeng2@illinois.edu>
2015-04-25 13:49:18 -0500

    Added Unit Testing UpdateAnime

--- unittests/UpdateAnimeTester.py
+++ unittests/UpdateAnimeTester.py
@@ -1,6 +1,41 @@
 import unittest
-
+import json
 from unittests import BaseMalbTester
+
+def decode_json(s):
+    return json.loads(s.data.decode('utf-8'))
+
+VALID_FORM_DATA = {
+    'malId': 1,
+    'myScore': 10,
+    'myStatus': 1,
+    'myEpisodes': 20,
+    'myStartDate': '01/20/2001',
+    'myEndDate': '03/02/1991'
+
+}
+
+VALID_INVALID_FORM_DATA = {
+    'malId': 1,
+    'myScore': 10,
+    'myStatus': 1,
+    'myEpisodes': 20,
+    'myStartDate': '01/20/2001',
+    'myEndDate': '3/4/15'
+
+}
+
+INVALID_FORM_DATA = {
+    'malId': 1,
+    'myScore': 10,
+    'myStatus': 1,
+    'myEpisodes': 20,
+    'myStartDate': '01/20/91',
+    'myEndDate': '555555'
+
+}
+
+
 
 OPTIONS_SCORE = {
     0: '<option selected value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select>',
@@ -77,6 +112,11 @@ UPDATE_DATA_INVALID_EPISODE = {
     'edit_form-myEpisodes_2': 600,
 }
 
+UPDATE_DATA_VALID_START_DATE = {
+    'edit_form-startDateStart_0': '12/12/2010',
+    'edit_form-startDateStart_1': '02/01/1992',
+    'edit_form-startDateStart_2': '11/06/2009',
+}
 
 class UpdateAnimeTest(BaseMalbTester.BaseMalbTest):
 
@@ -158,6 +198,79 @@ class UpdateAnimeTest(BaseMalbTester.BaseMalbTest):
         self.assertTrue('<input id="edit_form-myEpisodes_0" name="edit_form-myEpisodes_0" placeholder="1-26" type="text" value="-1">' in page)
         self.assertTrue('<input id="edit_form-myEpisodes_1" name="edit_form-myEpisodes_1" placeholder="1-24" type="text" value="400">' in page)
         self.assertTrue('<input id="edit_form-myEpisodes_2" name="edit_form-myEpisodes_2" placeholder="1-74" type="text" value="600">' in page)
+
+    def test_start_valid_date(self):
+        """Test update start date"""
+        self.login()
+        self.navigate_to('/update_anime')
+
+        s0 = self.submit_to('/update_anime', VALID_FORM_DATA)
+        print(s0.data)
+        print(type(s0))
+        print(dir(s0))
+
+        page = self.navigate_to('/updateanime').data.decode('utf-8')
+
+        self.logout()
+        self.assertEqual(s0._status_code, 200)
+        self.assertIn('01/20/2001', page)
+
+    def test_end_valid_date(self):
+        """Test update end date"""
+        self.login()
+        self.navigate_to('/update_anime')
+
+        s0 = self.submit_to('/update_anime', VALID_FORM_DATA)
+
+
+        page = self.navigate_to('/updateanime').data.decode('utf-8')
+
+        self.logout()
+        self.assertEqual(s0._status_code, 200)
+        self.assertIn('03/02/1991', page)
+
+    def test_start_invalid_date(self):
+        """Test update invalid start date"""
+        self.login()
+        self.navigate_to('/update_anime')
+
+        s0 = self.submit_to('/update_anime', INVALID_FORM_DATA)
+
+
+        page = self.navigate_to('/updateanime').data.decode('utf-8')
+
+        self.logout()
+#        self.assertEqual(s0._status_code, 200)
+        self.assertNotIn('01/20/91', page)
+
+    def test_end_invalid_date(self):
+        """Test update invalid end date"""
+        self.login()
+        self.navigate_to('/update_anime')
+
+        s0 = self.submit_to('/update_anime', INVALID_FORM_DATA)
+
+
+        page = self.navigate_to('/updateanime').data.decode('utf-8')
+
+        self.logout()
+#        self.assertEqual(s0._status_code, 200)
+        self.assertNotIn('3/4/15', page)
+
+    def test_start_valid_end_invalid_date(self):
+        """Test update invalid end date"""
+        self.login()
+        self.navigate_to('/update_anime')
+
+        s0 = self.submit_to('/update_anime', VALID_INVALID_FORM_DATA)
+
+
+        page = self.navigate_to('/updateanime').data.decode('utf-8')
+
+        self.logout()
+#        self.assertEqual(s0._status_code, 200)
+#        self.assertIn('01/20/2001', page)
+        self.assertNotIn('555555', page)
 
 if __name__ == '__main__':
     unittest.main()
